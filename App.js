e8ec389cd1856079ce277796db9a1095d177ff2d
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-04-27 15:44:01 +0700

    Init

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+import BallToBox from './animations/BallToBox'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+class Menu extends React.Component {
+
+  onPress = (viewName) => () => {
+    this.props.navigation.navigate(viewName)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button onPress={this.onPress('BallToBox')} title="Drop ball to box"></Button>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Menu
+  },
+  BallToBox: {
+    screen: BallToBox
+  }
+}, {
+  initialRouteName: "Home"
+});
+
+export default createAppContainer(AppNavigator);
---
+++ animations/BallToBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, Text } from 'react-native'
+
+export default class BallToBox extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Ball to box view</Text>
+      </View>
+    )
+  }
+}
